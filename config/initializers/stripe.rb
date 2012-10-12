0690e409eccaea9b93fed16c6eee7f7cd2915447
Siddarth Chandrasekaran <chandrasekaran.siddarth@gmail.com>
2012-10-12 00:28:53 -0700

    Raise if payments mode is not set.

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -2,4 +2,6 @@ if Settings.stripe_payments_mode == 'test'
   Stripe.api_key = Settings.stripe_test_secret_key
 elsif Settings.stripe_payments_mode == 'live'
   Stripe.api_key = Settings.stripe_live_secret_key
+else
+  raise "Stripe payments mode (live v. test) is not set in settings.yml."
 end
